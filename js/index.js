23c4fcc2dab27c6254769f0d9981045ebdf3fb43
Askary <askary18@hotmail.com>
2014-09-24 12:29:59 -0500

    probar conectividad

--- js/index.js
+++ js/index.js
@@ -16,6 +16,19 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+function checkConnection() {
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    alert('Connection type: ' + states[networkState]);
+} 
 var app = {
     // Application Constructor
     initialize: function() {
@@ -34,6 +47,8 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+		 networkState = navigator.connection.type;
+        checkConnection();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
